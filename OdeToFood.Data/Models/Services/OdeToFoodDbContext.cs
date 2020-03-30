972df98b015dc52c3daba3cf91274980c60cf326
Fernando do Carmo Teixeira <fernando.teixeira6@outlook.com>
2020-03-30 16:22:15 -0300

    Add project files.

---
+++ OdeToFood.Data/Models/Services/OdeToFoodDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Models.Services
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
